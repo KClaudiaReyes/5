6c51b35847ed6330d7553e5371644fd66a5be83d
clauR <creyesccf@gmail.com>
2026-03-04 19:39:39 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <string, int  > registro = new Dictionary<string, int>();
+
+            registro.Add("Marlon",18587);
+            registro.Add("Leon ", 49424);
+            registro.Add("Brisa", 202605);
+            registro.Add("Marta", 199526);
+            registro.Add("Enrique",200514);
+        }
+    }
+}
